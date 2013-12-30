277e1da09dbdd7df65125042b80d21ad39156d83
byzgaev <artem.byzgaev@kodep.ru>
2013-12-30 09:01:55 +0400

    posts update give one request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
-class PostsController < ApplicationController
+class PostsController < InheritedResources::Base
   before_filter :prepare_new_post, only: [:index]
-
+  respond_to :js, only: :create
   private
   def prepare_new_post
     @new_post = Post.new
